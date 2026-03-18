a11df482df853f081b018d7e33a61953742cb084
Smatt743 <wednesdaydarts.ashford@gmail.com>
2026-03-18 10:09:51 +0000

    Delete layout.js

--- layout.js
+++
@@ -1,3 +0,0 @@
-import Shell from '@/components/Shell';
-export const metadata = { title:'Wednesday 5 a Side Darts League', description:'League tables, fixtures, squads and player stats.' };
-export default function RootLayout({children}){ return <html lang="en"><body style={{margin:0}}><Shell>{children}</Shell></body></html>; }
